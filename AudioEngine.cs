34026b00d4efcfdef72de153c586f847c8bb15f0
Dvoryadkin Pavel <dvoryadkinpavel@mail.ru>
2021-04-03 18:17:57 +0700

    scroll works

--- AudioEngine.cs
+++ AudioEngine.cs
@@ -1,12 +1,14 @@
-using System.Transactions;
 using System;
 using System.Diagnostics;
 using SFML.Audio;
+using System.Threading.Tasks;
+using SFML.System;
 
 namespace AudioEngine
 {
     public class AudioPlayer
     {
+        public Task MP3Loading;
         private string _directory;
         private SoundBuffer _buffer;
         private Sound _sound;
@@ -17,43 +19,68 @@ namespace AudioEngine
             if(!fileName.Contains(".mp3"))
             {                
                 _buffer = new SoundBuffer(fileName);
-                              
+                _sound = new Sound();
+                _sound.SoundBuffer = _buffer;                
             }
             else
             {
                 LoadToBuffer(fileName);
-                _buffer = new SoundBuffer("buffer.ogg"); 
             }
+            
+        }
+        public void OpenBuffer()
+        {
+            _buffer = new SoundBuffer("buffer.ogg");
             _sound = new Sound();
-            _sound.SoundBuffer = _buffer;  
+            _sound.SoundBuffer = _buffer;   
         }
         private void LoadToBuffer(string fileName)
         {
-            string program = "ffmpeg";
-                var pi = new ProcessStartInfo("/bin/bash")
-                {
-                    Arguments = $"-y -i \"{fileName}\" buffer.ogg",
-                    WorkingDirectory = _directory,
-                    FileName = "ffmpeg",
-                    Verb = "OPEN",
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false
-                };
-                Process p = new Process();
-                p.StartInfo = pi;
-                p.Start();
-                String error = p.StandardError.ReadToEnd();
-                String output = p.StandardOutput.ReadToEnd() + pi.ToString();
-                p.WaitForExit();     
+            var pi = new ProcessStartInfo("/bin/bash")
+            {
+                Arguments = $"-y -i \"{fileName}\" buffer.ogg",
+                WorkingDirectory = _directory,
+                FileName = "ffmpeg",
+                Verb = "OPEN",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+            Process p = new Process();
+            p.StartInfo = pi;
+            p.Start();
+            MP3Loading = p.WaitForExitAsync();     
         }
         private string _fileName;
-        public string FileName
+        public float Duration
         {
             get
             {
-                return _fileName;
+                if(_buffer != null) return _buffer.Duration.AsSeconds();
+                else return 0;
+            }
+        }
+        public float Position
+        {
+            get
+            {
+                return _sound.PlayingOffset.AsSeconds();
+            }
+            set
+            {
+                _sound.PlayingOffset = Time.FromSeconds(value);
+            }
+        }
+        public float Volume
+        {
+            get
+            {
+                return _sound.Volume;
+            }
+            set
+            {
+                _sound.Volume = value;
             }
         }
         public void Play()
--- MainWindow.cs
+++ MainWindow.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using System;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
@@ -12,8 +13,9 @@ namespace SharpPlayer
         [UI] private Button _buttonPause = null;
         [UI] private Button _buttonStop = null;
         [UI] private Button _buttonOpen = null;
+        [UI] private Scrollbar _scroll = null;
         private AudioPlayer _player;
-
+        private float _currentVolume;
 
         public MainWindow() : this(new Builder("MainWindow.glade")) { }
 
@@ -26,7 +28,8 @@ namespace SharpPlayer
             _buttonPause.Clicked += ButtonPause_Clicked;
             _buttonStop.Clicked += ButtonStop_Clicked;
             _buttonOpen.Clicked += ButtonOpen_Clicked;
-            _labelFileName.Text = "Выберете файл";
+            _scroll.ValueChanged += Scrollbar_Changed;
+            new Task(()=>Scrollbar_Update()).Start();
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
@@ -37,7 +40,23 @@ namespace SharpPlayer
         {
             try
             {
-                _player = new AudioPlayer(fileName);
+                _player = new AudioPlayer(fileName);                
+                if(_player.MP3Loading != null) 
+                {
+                    _buttonOpen.Hide();
+                    _buttonPause.Hide();
+                    _buttonPlay.Hide();
+                    _buttonStop.Hide();
+                    _scroll.Hide();
+                    _labelFileName.Text = "Конвертация MP3 в OGG";                    
+                    _player.MP3Loading.Wait();
+                    _player.OpenBuffer();
+                    _buttonOpen.Show();
+                    _buttonPause.Show();
+                    _buttonPlay.Show();
+                    _buttonStop.Show();
+                    _scroll.Show();
+                }
                 _labelFileName.Text = fileName;
             }
             catch(Exception ex)
@@ -64,13 +83,40 @@ namespace SharpPlayer
 			fcd.AddButton (Gtk.Stock.Open, Gtk.ResponseType.Ok);
 			fcd.DefaultResponse = Gtk.ResponseType.Ok;
 			fcd.SelectMultiple = false;
-
 			Gtk.ResponseType response = (Gtk.ResponseType) fcd.Run ();
-			if (response == Gtk.ResponseType.Ok)
-                if(_player != null) _player.Stop();
-				LoadFile(fcd.Filename);
-			fcd.Dispose ();
+			if (response == Gtk.ResponseType.Ok) 
+            {           
+                if(_player != null) 
+                {
+                    _player.Stop();
+                    _player = null;
+                }
+                new Task(() => LoadFile(fcd.Filename)).Start();
+                fcd.Dispose ();
+            }
+			
 
+        }
+        private void Scrollbar_Update()
+        {
+            
+            while(true)
+            {
+                if((_player != null) && (_player.Duration != 0))
+                {
+                    _scroll.SetRange(0,_player.Duration);
+                    _scroll.Adjustment.Value = _player.Position;                    
+                }
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+        private void Scrollbar_Changed(object sender, EventArgs a)
+        {            
+            if(_player != null)
+            {
+                _scroll.SetRange(0,_player.Duration);
+                _player.Position = (float)_scroll.Adjustment.Value;                    
+            }
         }
     }
 }
